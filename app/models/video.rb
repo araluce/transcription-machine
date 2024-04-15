b2c5b83bccb29b9672d2a21631bc5c4d39f4c6cc
araluce <alvaro.fernandez@crisalix.com>
2024-04-15 21:40:40 +0200

    Automatically transcribe after commit video

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,14 @@
 class Video < ApplicationRecord
+  scope :captioned, -> { where(captions: true) }
   scope :uncaptioned, -> { where(captions: false) }
+
+  after_commit :transcribe, on: %i[create update], unless: :captions?
 
   def download_url
     "https://#{Rails.application.credentials.dig(:bunny, :zone)}.b-cdn.net/#{guid}/play_240p.mp4"
+  end
+
+  def transcribe
+    TranscribeJob.perform_later(self)
   end
 end
